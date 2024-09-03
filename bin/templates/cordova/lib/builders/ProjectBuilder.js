5a3d93c771e50014c36dcaf4b7a0620c0a3088d8
Ashera Cordova <ashera.cordova@gmail.com>
2024-09-03 23:11:56 +0530

    Revert "Upgrade of ashera to support android latest 12.0.0 version."
    This reverts commit 53505b622f303a407b412c86d6cf17ea76710bcf.

--- bin/templates/cordova/lib/builders/ProjectBuilder.js
+++ bin/templates/cordova/lib/builders/ProjectBuilder.js
@@ -185,7 +185,7 @@ class ProjectBuilder {
         }
 
         // get project name cdv-gradle-config.
-        const cdvGradleConfig = CordovaGradleConfigParserFactory.create(path.join(this.root, '../../platforms/android/'));
+        const cdvGradleConfig = CordovaGradleConfigParserFactory.create(this.root);
         const projectName = cdvGradleConfig.getProjectNameFromPackageName();
 
         // Remove the proj.id/name- prefix from projects: https://issues.apache.org/jira/browse/CB-9149
